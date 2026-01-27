609d146e5ebd437b871a56934439ed5ffdf8c54b
AndreLouisSt <stephantandrelouis@gmail.com>
2026-01-27 13:44:21 +0000

    Arfficheage cercle

--- src/Cercle.cpp
+++ src/Cercle.cpp
@@ -40,6 +40,23 @@ void Cercle::setCouleur(CouleurCercle couleur)
 // Méthodes
 void Cercle::AffichageCercle(int Pos_X, int Pos_Y)
 {
+    std::string couleurCase;
+    switch (couleur)
+    {
+    case CouleurCercle::bleu:
+        couleurCase = "\033[44m";
+        break;
+    case CouleurCercle::jaune:
+        couleurCase = "\033[43m";
+        break;
+    case CouleurCercle::rouge:
+        couleurCase = "\033[41m";
+        break;
+    case CouleurCercle::vert:
+        couleurCase = "\033[42m";
+        break;
+    }
+
     switch (taille)
     {
     case TailleCercle::petite:
@@ -47,16 +64,75 @@ void Cercle::AffichageCercle(int Pos_X, int Pos_Y)
         {
             for (int j = Pos_Y - 2; j <= Pos_Y + 2; j++)
             {
-                if (i == Pos_X-2 && j == Pos_Y-2) { std::cout << "╔"; } else if (i == Pos_X+2 && j == Pos_Y-2) { std::cout <<}
+                if (i == Pos_X - 2 && j == Pos_Y - 2)
+                {
+                    std::cout << couleurCase << "╔";
+                }
+                else if (i == Pos_X + 2 && j == Pos_Y - 2)
+                {
+                    std::cout << couleurCase << "╗";
+                }
+                else if (i == Pos_X - 2 && j == Pos_Y + 2)
+                {
+                    std::cout << couleurCase << "╚";
+                }
+                else if (i == Pos_X + 2 && j == Pos_Y + 2)
+                {
+                    std::cout << couleurCase << "╝";
+                }
+                else if (j == Pos_Y - 2 || j == Pos_Y + 2)
+                {
+                    std::cout << couleurCase << "═";
+                }
+                else if (i == Pos_X - 2 || i == Pos_X + 2)
+                {
+                    std::cout << couleurCase << "║";
+                }
+                else
+                {
+                    std::cout << " ";
+                }
             }
         }
-    }"═"
-    break;
-case TailleCercle::moyenne:
-
-    break;
-case TailleCercle::grande:
-
-    break;
-}
+        break;
+    case TailleCercle::moyenne:
+        for (int i = Pos_X - 1; i <= Pos_X + 1; i++)
+        {
+            for (int j = Pos_Y - 1; j <= Pos_Y + 1; j++)
+            {
+                if (i == Pos_X - 1 && j == Pos_Y - 1)
+                {
+                    std::cout << couleurCase << "╔";
+                }
+                else if (i == Pos_X + 1 && j == Pos_Y - 1)
+                {
+                    std::cout << couleurCase << "╗";
+                }
+                else if (i == Pos_X - 1 && j == Pos_Y + 1)
+                {
+                    std::cout << couleurCase << "╚";
+                }
+                else if (i == Pos_X + 1 && j == Pos_Y + 1)
+                {
+                    std::cout << couleurCase << "╝";
+                }
+                else if (j == Pos_Y - 1 || j == Pos_Y + 1)
+                {
+                    std::cout << couleurCase << "═";
+                }
+                else if (i == Pos_X - 1 || i == Pos_X + 1)
+                {
+                    std::cout << couleurCase << "║";
+                }
+                else
+                {
+                    std::cout << " ";
+                }
+            }
+        }
+        break;
+    case TailleCercle::grande:
+        std::cout << couleurCase << "O";
+        break;
+    }
 }
