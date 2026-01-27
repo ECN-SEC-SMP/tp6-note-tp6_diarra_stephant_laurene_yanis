e1242d4a43a92f41f2774d644bb5f3772a1a6ef3
Diarra <diopdiarra285@gmail.com>
2026-01-27 14:46:26 +0100

    correction des errors

--- include/Coup.hpp
+++ include/Coup.hpp
@@ -1,17 +1,26 @@
-#include iostream
+#include <iostream>
 using namespace std;
 
 #include "Cercle.hpp"
 #include "Case.hpp"
 
-Class Coup {
+class Coup {
+
 private:
     Cercle *cercle;
     Case *caseCible;
 
 public:
 
-
-
+/*-------------------------------------------------------------------------------*/
+// Constructeurs
+/*-------------------------------------------------------------------------------*/
+    Coup();
+    Coup(Cercle* cercle, Case* caseCible);
+//-------------------------------------------------------------------------------*/
+// Accesseurs
+/*-------------------------------------------------------------------------------*/
+    Cercle* getCercle();
+    Case* getCaseCible();
 
 };
