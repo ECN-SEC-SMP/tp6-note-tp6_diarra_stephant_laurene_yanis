4e80b6961440fe779d47dadb3b6d93a97a119242
AndreLouisSt <stephantandrelouis@gmail.com>
2026-01-27 13:13:39 +0000

    Plateau 3x3

--- include/Plateau.hpp
+++ include/Plateau.hpp
@@ -8,11 +8,7 @@
 class Plateau
 {
 private:
-    int l = 0;
-    int L = 0;
-    
     Case* cases[3][3];  // Pas possible de faire un tableau dynamique avec l et L
-    std::vector<std::vector<Case*>> cases1;  // Proposition Copilot
 
 public:
     // Constructeurs
--- src/Cercle.cpp
+++ src/Cercle.cpp
@@ -1,13 +1,16 @@
 #include "Cercle.hpp"
 #include <iostream>
+#include <string>
 
 // Constructeurs
-Cercle::Cercle() {
+Cercle::Cercle()
+{
     this->taille = TailleCercle::petite;
     this->couleur = CouleurCercle::rouge;
 }
 
-Cercle::Cercle(TailleCercle taille, CouleurCercle couleur) {
+Cercle::Cercle(TailleCercle taille, CouleurCercle couleur)
+{
     this->taille = taille;
     this->couleur = couleur;
 }
@@ -37,5 +40,23 @@ void Cercle::setCouleur(CouleurCercle couleur)
 // Méthodes
 void Cercle::AffichageCercle(int Pos_X, int Pos_Y)
 {
-    ;
+    switch (taille)
+    {
+    case TailleCercle::petite:
+        for (int i = Pos_X - 2; i <= Pos_X + 2; i++)
+        {
+            for (int j = Pos_Y - 2; j <= Pos_Y + 2; j++)
+            {
+                if (i == Pos_X-2 && j == Pos_Y-2) { std::cout << "╔"; } else if (i == Pos_X+2 && j == Pos_Y-2) { std::cout <<}
+            }
+        }
+    }"═"
+    break;
+case TailleCercle::moyenne:
+
+    break;
+case TailleCercle::grande:
+
+    break;
+}
 }
