c07da22c513f338884e3c8146583aa530075d851
Diarra <diopdiarra285@gmail.com>
2026-01-27 11:43:25 +0100

    ajout spec fonctions

--- src/Controleur.hpp
+++ src/Controleur.hpp
@@ -48,12 +48,30 @@ class Controleur {
 /*-------------------------------------------------------------------------------*/
 // Méthodes de la classe
 /*-------------------------------------------------------------------------------*/
+/**
+ * @brief 
+ * 
+ */
     void runPartie();
 
+/**
+ * @brief Sélectionne le joueur suivant
+ * 
+ */
     void nextJoueur();
 
+/**
+ * @brief gère le tour du joueur courant
+ * 
+ */
     void gererTour(); //choix joueur + gestion de son tour
-
+    
+/**
+ * @brief Vérifie via le plateau si le coup proposé par le joueur est valide
+ * Si le coup est valide, le coup est joué sur le plateau
+ * sinon on demande au joueur de rejouer
+ * @param joueur 
+ */
     void jouerCoup(Joueur* joueur); //gestion du coup joué par le joueur
 
     // Destructeur
