dea7270a8029e0b746fd017e1fc4aa208033079a
Laurene Bernier <lrn.bernier@gmail.com>
2026-02-03 20:52:14 +0100

    update fonction jouer mais reste à améliorer

--- src/JoueurHumain.cpp
+++ src/JoueurHumain.cpp
@@ -34,24 +34,21 @@ JoueurHumain::JoueurHumain(Plateau *p, CouleurCercle c, int id) : Joueur(p, c, i
  */
 Coup JoueurHumain::Jouer()  {
     bool retry = 1;
-    Cercle* newCercle;
-    Case* newCaseCible;
-    Coup newCoup(newCercle, newCaseCible);
     bool coupDejaFait = 0;
     TailleCercle newTailleCercle;
+    int ligne=0; 
+    int colonne=0;
+    std::string taille;
 
     while(retry){
         retry = 0;
-        int ligne=0; 
-        int colonne=0;
+        
         std::cout<<"Quel est ton coup ?"<<std::endl;
-        std::cout<<"1 2 : colonne 1 & ligne 2"<<std::endl;
-        std::cin>>colonne>>ligne; // espace = séparateur
+        std::cout<<"1 2 : colonne 1 & ligne 2 /!/ à lespace"<<std::endl;
+        std::cin>>colonne>>ligne; // il faut un espace = séparateur
 
-        std::string taille;
         std::cout<<"Quelle taille ? S:small M:medium L:large"<<std::endl;
         std::cin>>taille; // espace = séparateur
-
 
         if((ligne>3 || ligne<=0)){
             std::cout<<"Il faut réunir votre équipe avant d'aller plus loin"<<std::endl;
@@ -65,23 +62,24 @@ Coup JoueurHumain::Jouer()  {
             std::cout<<"Veuillez respecter l'ensemble de définition : S-M-L"<<std::endl;
             retry=1;
         }
-
         // vérif la disponibilité
         if(plateau->getCase(colonne, ligne)->getCercles()[static_cast<int>(newTailleCercle)] != nullptr){
             coupDejaFait = 1;
         }
 
-        // si la propal est bonne, l'enregistrer
         if(ligne<3 && ligne>=0 && colonne<3 && colonne>=0 && (taille=="S" || taille=="M" || taille=="L") && !(coupDejaFait)){
-            if(taille == "S") newTailleCercle = petite;
-            else if(taille == "M") newTailleCercle = moyenne;
-            else newTailleCercle = grande;
-
-            std::cout<<"Votre coup est bien enregistré"<<std::endl;
-            newCercle->setTaille(newTailleCercle);
-            newCaseCible->setPosX(colonne);
-            newCaseCible->setPosY(ligne);
+            retry = 0;
         }
     }
+    // si la propal est bonne, l'enregistrer
+    if(taille == "S") newTailleCercle = petite;
+    else if(taille == "M") newTailleCercle = moyenne;
+    else newTailleCercle = grande;
+
+    std::cout<<"Votre coup est bien enregistré"<<std::endl;
+    Cercle* c1 = plateau->getCase(colonne, ligne)->getCercles()[newTailleCercle];
+    Case* c2 = plateau->getCase(colonne, ligne);
+    Coup newCoup( c1, c2);
+
     return newCoup;
 }
